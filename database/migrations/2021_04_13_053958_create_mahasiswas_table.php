3ee55144fd5c70cad533763f02cd4e94629639f0
naufalyukafi <naufalyukafi10@gmail.com>
2021-04-13 14:04:13 +0700

    copy(jobsheet9): create table migrations

---
+++ database/migrations/2021_04_13_053958_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim', 20)->primary();
+            $table->string('name', 50)->nullable();
+            $table->string('kelas', 20)->nullable();
+            $table->string('jurusan', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
